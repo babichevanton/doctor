d37e395ef2b45eeaa0ad58716e8609707ddb24e8
Anton Babichev <bav.dandellion@gmail.com>
2014-09-19 14:57:06 +0400

    Doctor project. Base frame. Wider assortiment of hedges and qualifier. Improved replace pattern for patient-phrase

---
+++ doctor.rkt
@@ -0,0 +1,83 @@
+#lang scheme/base
+(define (visit-doctor name)
+  (define (doctor-driver-loop name)
+    (define (reply user-response)
+      (define (change-person phrase)
+           (many-replace '(
+                          (are-change am)
+                          (your-change my)
+                          (yourself-change myself)
+                          (you-change i)
+                          (me you)
+                          (am are) 
+                          (my your)
+                          (myself yourself)
+                          (i you)
+                          (are are-change)
+                          (your your-change)
+                          (yourself yourself-change)
+                          (you you-change)
+                          ) phrase))
+
+      (define (qualifier)
+        (pick-random '((you seem to think)
+                       (you feel that)
+                       (why do you believe)
+                       (why do you say)
+                       (what makes you think that)
+                       )))
+
+      (define (hedge)
+        (pick-random '((please go on)
+                       (please tell me more about this)
+                       (many people have the same sorts of feelings)
+                       (many of my patients have told me the same thing)
+                       (please continue)
+                       )))
+      
+      (cond ((fifty-fifty)
+             (append (qualifier) (change-person user-response)))
+            (else (hedge))))
+
+    (newline)
+    (print '**)
+    (let ((user-response (myread '())))
+      (cond ((equal? user-response '(goodbye))
+             (printf "Goodbye, ~a!\n" name)
+             (print '(see you next week)))
+            (else (print (reply user-response))
+                  (doctor-driver-loop name)))))
+
+  (printf "Hello, ~a!\n" name)
+  (print '(what seems to be the trouble?))
+  (doctor-driver-loop name))
+
+(define (fifty-fifty)
+  (= (random 2) 0))
+
+(define (replace pattern replacement lst)
+  (cond ((null? lst) '())
+        ((equal? (car lst) pattern)
+         (cons replacement
+               (replace pattern replacement (cdr lst))))
+        (else
+         (cons (car lst)
+               (replace pattern replacement (cdr lst))))))
+
+(define (pick-random lst)
+  (list-ref lst (random (length lst))))
+
+(define (many-replace replacement-pairs lst)
+        (cond ((null? replacement-pairs) lst)
+              (else (let ((pat-rep (car replacement-pairs)))
+                      (replace (car pat-rep)
+                               (cadr pat-rep)
+                               (many-replace (cdr replacement-pairs) lst))))))
+
+(define (myread input) 
+  (let ((token (read)))
+    (cond ((eof-object? token)
+             (newline)
+             (reverse input))
+           (else
+             (myread (cons token input))))))
